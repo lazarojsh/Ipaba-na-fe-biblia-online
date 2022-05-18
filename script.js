95cbe8172f4543e2abf861bf9a9c756533eb2a3d
Lazaro Martins <lazaromartins012345@gmail.com>
2022-05-18 18:55:01 -0300

    Ipaba na fé

---
+++ script.js
@@ -0,0 +1,11 @@
+function buscar() {
+    let nav= document.getElementById('Buscar')
+    let resp= document.getElementById('res')
+    if(nav.value.length == 0) {
+        alert('Busca disponível no momento!')
+    } else {
+        alert('Busca disponível em breve!')
+    }
+
+    
+}
